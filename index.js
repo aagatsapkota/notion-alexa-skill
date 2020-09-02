d2eef67813758309e1b4b252adcf5c3dd171ea51
Aagat Sapkota <ars9@williams.edu>
2020-09-02 00:16:35 -0400

    everything is working now!

--- index.js
+++ index.js
@@ -2,16 +2,15 @@
 
 const Alexa = require('ask-sdk-core')
 const fetch = require('node-fetch')
-const file = require('./TestIssues.json')
 require('dotenv').config()
 
-// const { API_KEY } = process.env
-// const { GIT_API_KEY } = process.env
 const { GIT_API_KEY } = process.env
 const user = 'aagatsapkota'
 const repo = 'sls-meetup-alexa-skill'
-let issueName = ''
 const SKILL_NAME = 'Create Issue'
+
+// create project, and tell what project and status the issue is on
+// second call create a project card, it accepts issue ID, pass status and create project entry
 
 // Handlers
 const LaunchRequestHandler = {
@@ -28,25 +27,62 @@ const LaunchRequestHandler = {
   }
 }
 
+// const createIssue takes a tile and returns an issue
+// take the fetch function in here
+
+const createIssue = async (issueName) => {
+  const issueResponse = await fetch(`https://api.github.com/repos/${user}/${repo}/issues`, {
+    method: 'post',
+    body: JSON.stringify({
+      title: issueName
+    }),
+    headers: { 'Content-Type': 'application/json', Authorization: `token ${GIT_API_KEY}` }
+  })
+  return issueResponse.json()
+}
+const getProject = async () => {
+  const projectName = await fetch(`https://api.github.com/repos/${user}/${repo}/projects`, {
+    method: 'get',
+    headers: { Authorization: `token ${GIT_API_KEY}`, Accept: 'application/vnd.github.inertia-preview+json' }
+  })
+  return projectName.json()
+}
+const getColumn = async () => {
+  const project = await getProject()
+  const projectId = project[0].id
+  const column = await fetch(`https://api.github.com/projects/${projectId}/columns`, {
+    method: 'get',
+    headers: { Authorization: `token ${GIT_API_KEY}`, Accept: 'application/vnd.github.inertia-preview+json' }
+  })
+  return column.json()
+}
+// issueID  690637232
+// projectID 5381051
+// columnID 10656169
+const addProjectCard = async (issue) => {
+  const column = await getColumn()
+  const columnId = column[0].id
+  const createCard = await fetch(`https://api.github.com/projects/columns/${columnId}/cards`, {
+    method: 'post',
+    body: JSON.stringify({
+      note: issue.title
+    }),
+    headers: { Authorization: `token ${GIT_API_KEY}`, Accept: 'application/vnd.github.inertia-preview+json' }
+  })
+
+  return createCard.json()
+}
+
 const MeetupIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'MeetupIntent'
+      && handlerInput.requestEnvelope.request.intent.name === 'MeetupIntent'
   },
   async handle(handlerInput) {
-    issueName = handlerInput.requestEnvelope.request.intent.slots.Name.value
-    const speechText = `Hi have created your issue called ${issueName}`
-    // process.env.TOKEN
-
-    fetch(`https://api.github.com/repos/${user}/${repo}/issues`, {
-      method: 'post',
-      body: JSON.stringify(file),
-      headers: { 'Content-Type': 'application/json', Authorization: `token ${GIT_API_KEY}` }
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(`Issue created at ${json.url}`)
-      })
+    const issueName = handlerInput.requestEnvelope.request.intent.slots.Name.value
+    const issue = await createIssue(issueName)
+    const projectCard = await addProjectCard(issue)
+    const speechText = `Created issue ID ${issue.number} with title ${issue.title} `
 
     return handlerInput.responseBuilder
       .speak(speechText)
